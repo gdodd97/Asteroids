d440fac50a0d67dfaa1655d133623fee6ea76b3b
gdodd97 <gdodd97@gmail.com>
2025-07-25 08:46:00 -0400

    Added player object and rotation logic to player

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
+from player import *
 from constants import *
+
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x,y)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,8 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
