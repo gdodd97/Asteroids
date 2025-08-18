7d4e0799590c4cc537f20bcab0cf406b10a3e493
gdodd97 <gdodd97@gmail.com>
2025-08-17 21:09:01 -0400

    Added asteroids, asteroid field, and random movement.

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 import pygame
 from player import *
 from constants import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
+
+    #initial variables and groups
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x,y)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroidField = AsteroidField()
+
+
+
+    #print screen
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+
+    #game loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+
         
 
 if __name__ == "__main__":
